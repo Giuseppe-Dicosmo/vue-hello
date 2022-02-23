3ea77c33ea9f1847514be1c1849c6d966445ce25
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-02-23 20:02:25 +0100

    end

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const container = new Vue({
+  el: '#container',
+  data: {
+    titolo: 'Immagini a caso',
+    immagine: 'https://picsum.photos/500/500',
+    transform: 'text-transform',
+    center: 'center-text'
+  }
+});
+console.log("🚀  container", container)
